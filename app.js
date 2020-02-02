2c7a7c25be7b5aab20f60d677e7d94d2fd93bff3
tumendorj <ttumendorj@gmail.com>
2020-02-02 20:47:51 +0800

    orlogo zarlaga hadgalah

--- app.js
+++ app.js
@@ -20,7 +20,28 @@ var uiController = (function() {
   };
 })();
 // санхүүтэй ажиллах контроллер
-var financeController = (function() {})();
+var financeController = (function() {
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItem: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  };
+})();
 // програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
   var ctlAddItem = function() {
