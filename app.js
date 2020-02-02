7ec0fb2c3d0b8f848f0fe7ac658eb800622b91f6
tumendorj <ttumendorj@gmail.com>
2020-02-02 20:31:08 +0800

    orlogo zarlaga hadgalah object zarlah

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 // дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+  var DOMstrings = {
+    type: "add__type",
+    description: ".add__description",
+    acc: ".add__value",
+    btn: ".add__btn"
+  };
+  return {
+    getValues: function() {
+      return {
+        type: document.querySelector(DOMstrings.type).value,
+        description: document.querySelector(DOMstrings.type).value,
+        acc: document.querySelector(DOMstrings.acc).value
+      };
+    },
+    getDOM: function() {
+      return DOMstrings;
+    }
+  };
+})();
 // санхүүтэй ажиллах контроллер
 var financeController = (function() {})();
 // програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  var ctlAddItem = function() {
     // 1. оруулах өгөгдлийг дэлгэцээс авах
     // 2. олсон өгөгдлөө санхүүгийн контроллерт дамжуулж тэнд нь хадгална
     // 3. өгөгдлүүдийг дэлгэцэнд гаргана
     // 4. төсвийг тооцоолно
     // 5. тооцооллыг дэлгэцлэнэ
-  });
-  document.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") console.log(event);
-  });
+  };
+  var setupEventListeners = function() {
+    var DOM = uiController.getDOM();
+    document.querySelector(DOM.btn).addEventListener("click", function() {
+      ctlAddItem();
+    });
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13) ctlAddItem();
+    });
+  };
+  return {
+    init: function() {
+      setupEventListeners();
+    }
+  };
 })(uiController, financeController);
+appController.init();
